9ef757febfcf4b13b5b328254c0713478e7c3501
m21aie249 <mathur.9@iitj.ac.in>
2024-04-06 15:43:01 +0530

    Updated quicktest.sh for marker options

--- docker/quicktest.sh
+++ docker/quicktest.sh
@@ -16,13 +16,35 @@ elif [ $1 = "rtlsim" ]; then
 elif [ $1 = "end2end" ]; then
   echo "Running end2end test suite with no parallelism"
   pytest -k end2end
+elif [ $1 = "bnn_pynq" ]; then
+  echo "Running bnn_pynq test suite with no parallelism"
+  pytest -k bnn_pynq
+elif [ $1 = "slow" ]; then
+  echo "Running slow test suite with no parallelism"
+  pytest -k slow
+elif [ $1 = "vitis" ]; then
+  echo "Running vitis test suite with no parallelism"
+  pytest -k vitis
+elif [ $1 = "vivado" ]; then
+  echo "Running vivado test suite with no parallelism"
+  pytest -k vivado
+elif [ $1 = "util" ]; then
+  echo "Running util test suite with no parallelism"
+  pytest -k util
+elif [ $1 = "fpgadataflow" ]; then
+  echo "Running fpgadataflow test suite with no parallelism"
+  pytest -k fpgadataflow
 elif [ $1 = "full" ]; then
   echo "Running full test suite, each step with appropriate parallelism"
   $0 main;
   $0 rtlsim;
   $0 end2end;
   $0 bnn_pynq;
-  $0 bnn_zcu104;
+  $0 slow;
+  $0 vitis
+  $0 vivado
+  $0 util
+  $0 fpgadataflow
 else
   echo "Unrecognized argument to quicktest.sh"
 fi
